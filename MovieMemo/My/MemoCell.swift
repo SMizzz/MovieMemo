1f380bb8f695a256665224034d4c22865a46d514
SMizzz <shinn.mizzz@gmail.com>
2021-07-07 20:52:26 +0900

    MY VC CollectionView Setting

---
+++ MovieMemo/My/MemoCell.swift
@@ -0,0 +1,19 @@
+//
+//  MemoCell.swift
+//  MovieMemo
+//
+//  Created by 신미지 on 2021/07/07.
+//
+
+import UIKit
+
+class MemoCell: UICollectionViewCell {
+  
+  
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var imageBtn: UIButton!
+
+  @IBAction func imageBtnTap(_ sender: Any) {
+    print("image tapped!")
+  }
+}
---
+++ MovieMemo/My/MyViewController.swift
@@ -0,0 +1,50 @@
+//
+//  MyViewController.swift
+//  MovieMemo
+//
+//  Created by 신미지 on 2021/07/07.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+  
+  @IBOutlet weak var memoCollectionView: UICollectionView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureCollectionView()
+  }
+  
+  private func configureCollectionView() {
+    memoCollectionView.delegate = self
+    memoCollectionView.dataSource = self
+  }
+  
+  
+  @IBAction func filterBtnTap(_ sender: Any) {
+  }
+  
+  
+}
+
+extension MyViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 4
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    guard let cell = memoCollectionView.dequeueReusableCell(withReuseIdentifier: "MemoCell", for: indexPath) as? MemoCell else { return UICollectionViewCell() }
+    cell.imageBtn.setImage(UIImage(named: "harrypotter"), for: .normal)
+    cell.titleLabel.text = "해리포터와 불사조 기사단을 보고 나서"
+    return cell
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return CGSize(width: 330, height: 590)
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+    return 20.0
+  }
+}
